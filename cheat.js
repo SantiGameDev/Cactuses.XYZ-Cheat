d7c50dbf42b2d6f50fcda64da7f6e87df540d17a
SantiGameDev <93549981+SantiGameDev@users.noreply.github.com>
2022-02-24 15:09:32 -0500

    Update cheat.js

--- cheat.js
+++ cheat.js
@@ -1,25 +1,66 @@
-(function(ns){
+export default function cheat(ns){
+	if(window.__cheatsON)return 'You already turned on cheats!'
+	window.__cheatsON = true
+	const canvas = document.getElementById('gameCanvas')
 	const CSSViewModes = ["auto", "crisp-edges", "pixelated", "smooth", "high-quality"];
-	let poll=false, rapidfire=false, cssviewmode=0;
+	let poll = false, rapidfire = false, firing = false, cssviewmode = 2;
+	
+	addEventListener('keyup',(event) => {if(event.key.toLowerCase() == 'c')firing = false})
+	addEventListener('keydown',(event) => {if(event.key.toLowerCase() == 'c')firing = true})
+	
 	addEventListener('keyup',(event) => {
-		if(event.key=='z'){
-			poll=!poll;
-			console.log(`Fast Polling is ${poll?'ON':'OFF'}`)
-		}else if(event.key=='x'){
-			rapidfire=!rapidfire;
-			console.log(`Rapid Fire is ${rapidfire?'ON':'OFF'}`)
-		}else if(event.key=='c'){
-			cssviewmode=(cssviewmode>3)?0:(cssviewmode+1)
-			ns.canvas.style.imageRendering = CSSViewModes[cssviewmode]
-			console.log(`Graphics Mode is ${CSSViewModes[cssviewmode]}`)
-		}else if(event.key=='v'){
-			ns.levelBuilt=[];
-			console.log('Level Cleared!')
-		}
+		if(event.key.toLowerCase() != 'z')return;
+		const message = [
+			'Select which cheat to select:',
+			'0) Cancel',
+			`1) Fast Polling: ${poll}`,
+			`2) Rapid Fire: ${rapidfire}`,
+			`3) Graphics Mode: ${CSSViewModes[cssviewmode]}`,
+			'4) Clear Map Data (DANGEROUS!) (removes 99% of walls)'
+		].join('\n');
+	
+		switch(prompt(message, '0')){
+			case '0': return
+			case '1':
+				poll=!poll;
+				alert(`Fast Polling was set to ${poll?'ON':'OFF'}`)
+				break;
+			case '2':
+				rapidfire=!rapidfire;
+				alert(`Rapid Fire was set to ${rapidfire?'ON':'OFF'}`)
+				break;
+			case '3':
+				const selection = prompt([
+				'Select which mode to use:',
+				'0) Cancel',
+				'1) Auto (default for web pages)',
+				'2) Crisp Edges',
+				'3) Pixelated (default for Cactuses.xyz)',
+				'4) Smooth (not supported on all browsers)',
+				'5) High Quality (not supported on all browsers)'
+				].join('\n'))
+				if(0<selection<6){
+				cssviewmode = selection - 1
+				canvas.style.imageRendering = CSSViewModes[cssviewmode]
+				alert(`Graphics Mode was set to &{CSSViewModes[cssviewmode]}`)
+				}
+				break;
+			case '4':
+				const confirmation = confirm('Are you sure you want to do this? The map WILL NOT reload until next visit!')
+				if(confirmation){
+					ns.levelBuilt=[];
+					alert('Wow! I really thought you didn\'t have the guts to do that!')
+				}else{
+					alert('100 SELF RESTRAINT')
+				}
+				break;
+			default:
+				alert('Are you stupid? That\'s not even on the list!')
+			}
 	});
 	setInterval(()=>{
-		if(rapidfire){ns.cKeyPressed();ns.lastShot=0;}
+		if(rapidfire && firing){ns.cKeyPressed();ns.lastShot=0;}
 		if(poll){ns.lastUpload=0;}
 	},1)
-	return 'Cheats:\n\nx-FasterPolling\nx-Rapid Fire\nCycle View Modes\nClear Map (only works once)'
-})()
+	return 'Press "z" to open up cheat menu'
+}
