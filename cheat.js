1f79e7c88b8217e9a9721b38d97743459db14126
SantiGameDev <93549981+SantiGameDev@users.noreply.github.com>
2022-02-24 15:49:37 -0500

    Update cheat.js

--- cheat.js
+++ cheat.js
@@ -57,7 +57,10 @@ export default function cheat(ns){
 				}
 				break;
 			case '5':
+				useFullscreen=!useFullscreen;
 				styles.innerHTML = (!useFullscreen) ? '' : 'html, body, canvas {overflow: hidden;margin: 0 !important;padding: 0 !important;width:100%;height:100%;display:block;background-color: #000045;position: fixed;left:0;top:0;}'
+				alert(`True Fullscreen was set to ${useFullscreen?'ON':'OFF'}`)
+				break;
 			default:
 				alert('Are you stupid? That\'s not even on the list!')
 			}
