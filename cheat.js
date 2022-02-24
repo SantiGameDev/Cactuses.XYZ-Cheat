795b444cce2dcb22e274c3113346c9beea81b423
SantiGameDev <93549981+SantiGameDev@users.noreply.github.com>
2022-02-24 13:31:54 -0500

    Update cheat.js

--- cheat.js
+++ cheat.js
@@ -1,7 +1,6 @@
-(function(){
-	CSSViewModes = ["auto", "crisp-edges", "pixelated", "smooth", "high-quality"];
-	poll=rapidfire=false;
-	cssviewmode=0;
+(function(ns){
+	const CSSViewModes = ["auto", "crisp-edges", "pixelated", "smooth", "high-quality"];
+	let poll=false, rapidfire=false, cssviewmode=0;
 	addEventListener('keyup',(event) => {
 		if(event.key=='z'){
 			poll=!poll;
@@ -11,16 +10,16 @@
 			console.log(`Rapid Fire is ${rapidfire?'ON':'OFF'}`)
 		}else if(event.key=='c'){
 			cssviewmode=(cssviewmode>3)?0:(cssviewmode+1)
-			canvas.style.imageRendering = CSSViewModes[cssviewmode]
+			ns.canvas.style.imageRendering = CSSViewModes[cssviewmode]
 			console.log(`Graphics Mode is ${CSSViewModes[cssviewmode]}`)
 		}else if(event.key=='v'){
-			levelBuilt=[];
+			ns.levelBuilt=[];
 			console.log('Level Cleared!')
 		}
 	});
 	setInterval(()=>{
-		if(rapidfire){cKeyPressed();lastShot=0;}
-		if(poll){lastUpload=0;}
+		if(rapidfire){ns.cKeyPressed();ns.lastShot=0;}
+		if(poll){ns.lastUpload=0;}
 	},1)
 	return 'Cheats:\n\nx-FasterPolling\nx-Rapid Fire\nCycle View Modes\nClear Map (only works once)'
 })()
