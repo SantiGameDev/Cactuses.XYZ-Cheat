373f16c962d7f5fbad633a665f504eb86d1a2360
SantiGameDev <93549981+SantiGameDev@users.noreply.github.com>
2022-02-28 07:51:09 -0500

    e

--- cheat.js
+++ cheat.js
@@ -92,7 +92,6 @@ export default function cheat() {
 				if((prop == '0') || (prop == null))return;
 				const value = prompt(`Input new value for ${PlayerSettings.numberToPropName(prop)}`)
 				alert(Player.setProperty(prop, value))
-				
 				break;
 			default:
 				alert(`Are you stupid? '${setting}' is not on the list!`)
